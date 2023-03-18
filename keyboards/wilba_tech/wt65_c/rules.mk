a68c4fb0e8e43aa2adb3cc96589b74ef6c0c4af6
Nelson Tran <nelson@tran21.com>
2023-03-18 14:48:23 -0700

    Port WT65-C keyboard config
    Taken from wilba:wt_c_pr (bcef33514a122c626cce769edd9b7013541ac46e).
    Still an unmerged PR, so there might be pending changes missing here.

---
+++ keyboards/wilba_tech/wt65_c/rules.mk
@@ -0,0 +1,14 @@
+# Build Options
+CIE1931_CURVE = yes
+
+# Do not put the microcontroller into power saving mode
+# when we get USB suspend event. We want it to keep updating
+# backlight effects.
+OPT_DEFS += -DNO_SUSPEND_POWER_DOWN
+
+# project specific files
+SRC =	keyboards/wilba_tech/wt_main.c \
+		keyboards/wilba_tech/wt_rgb_backlight.c \
+		quantum/color.c \
+		drivers/led/issi/is31fl3731.c \
+		i2c_master.c
